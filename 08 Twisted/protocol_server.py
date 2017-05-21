feba66ff429e060ffeee8644c49b48436bb6e867
fauzanpahlawan <fauzan.pahlawan@gmail.com>
2017-05-21 23:27:10 +0700

    update

--- 08 Twisted/protocol_server.py
+++ 08 Twisted/protocol_server.py
@@ -1,18 +1,21 @@
 from twisted.internet import protocol, reactor
 
 # Definisikan class untuk protocol yg akan kita buat
-class EchoServer(protocol.Protocol) :
-	# Callback func ketika koneksi berhasil dibuat
-	def connectionMade(self) :
-		print "Koneksi baru berhasil dibuat"
 
-	# Callback func ketika ada message masuk dari client
-	def dataReceived(self, data):
-		print data
+
+class EchoServer(protocol.Protocol):
+    # Callback func ketika koneksi berhasil dibuat
+    def connectionMade(self):
+        print "Koneksi baru berhasil dibuat"
+
+    # Callback func ketika ada message masuk dari client
+    def dataReceived(self, data):
+        print data
+
 
 class EchoFactory(protocol.Factory):
-	def buildProtocol(self, address) :
-		return EchoServer()
+    def buildProtocol(self, address):
+        return EchoServer()
 
 reactor.listenTCP(7777, EchoFactory())
 reactor.run()
